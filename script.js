a1137c4f997aedcad7fea28be3fd28c7cdd72240
alef-fullstack <dev.alef25@gmail.com>
2026-05-20 15:52:40 -0300

    adicionando ENUM's

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ let humanScoreNumber = 0
 let machineScoreNumber = 0
 let empate = 0
 
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors',
+
+}
+
 const humanPlay = (humanChoise) => {
 
     playTheGame(humanChoise, machinePlay())
@@ -15,7 +22,7 @@ const humanPlay = (humanChoise) => {
 }
 
 const machinePlay = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
 
 
@@ -29,9 +36,9 @@ const playTheGame = (human, machine) => {
         empate++
         empateScore.innerHTML = empate
         result.innerHTML = "Deu empate! 🫤​"
-    } else if ((human === 'paper' && machine === 'rock') ||
-        (human === 'rock' && machine === 'scissors') ||
-        (human === 'scissors' && machine === 'paper')
+    } else if ((human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) ||
+        (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) ||
+        (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)
     ) {
         humanScoreNumber++
         humanScore.innerHTML = humanScoreNumber
